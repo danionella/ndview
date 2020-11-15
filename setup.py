014374c4e3e5a43276b8c6fefa18536052dfc69e
benju <benju@charite.de>
2020-11-15 22:35:04 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# see also https://blog.godatadriven.com/setup-py
+# python setup.py sdist bdist_wheel
+#
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ndview',
+    version='0.001dev',
+    description='Multidimensional array viewer widget for jupyter',
+    author='jlab.berlin',
+    url='https//github.com/danionella/ndview',
+    packages=['ndview', ], #find_packages(include=['exampleproject', 'exampleproject.*']),
+    license='mit',
+    python_requires='>=3.5',
+    install_requires=['matplotlib', 'ipywidgets', 'ipympl', 'numpy']
+)
