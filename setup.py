0c42c9fed9fc9897a9eb0d9eacb902b7f1be4289
Benjamin <bjudkewitz@users.noreply.github.com>
2020-11-16 08:37:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ndview',
-    version='0.01dev',
+    version='0.0.1dev',
     description='Multidimensional array viewer widget for jupyter',
     author='jlab.berlin',
     url='https//github.com/danionella/ndview',
